c31cb42e6385055ad1aac9546b2fb7385de34ffe
mkravchenko <traderxaoc@yande.ru>
2021-07-06 21:39:24 +0300

    dz55

--- 1.py
+++
@@ -1,6 +0,0 @@
-with open('1.txt', 'w', encoding='utf-8') as f:
-    while True:
-        line = input('Введите новую строку - ')
-        if not line:
-            break
-        f.write(f'{line}\n')
